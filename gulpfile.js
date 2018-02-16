ce933465e0ff358ffca53c62d08d860a089da363
ilebedev-ru <wapstyle@bk.ru>
2018-02-16 21:17:43 +0500

    ПП страницы индекс

--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("build/css"))
+    .pipe(gulp.dest("source/css"))
     .pipe(minify())
     .pipe(rename("style.min.css"))
     .pipe(gulp.dest("build/css"))
@@ -42,6 +42,7 @@ gulp.task("images", function () {
 
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
+    .pipe(webp({quality: 90}))
     .pipe(gulp.dest("build/img"));
 });
 
