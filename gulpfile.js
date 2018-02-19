ad9255f678a689259cc4a65800ea3ccd79a85dfc
ilebedev-ru <wapstyle@bk.ru>
2018-02-19 17:01:44 +0500

    перенес иконки соц сетей в спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -47,7 +47,11 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src(["source/img/logo-pink-*.svg", "source/img/icon-editor-*.svg"])
+  return gulp.src([
+    "source/img/logo-pink-*.svg",
+    "source/img/icon-editor-*.svg",
+    "source/img/icon-social-*.svg"
+    ])
     .pipe(svgstore({
       inlineSvg: true
     }))
